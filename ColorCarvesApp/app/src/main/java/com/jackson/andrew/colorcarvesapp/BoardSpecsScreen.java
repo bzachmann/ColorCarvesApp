6889f70944725a900624dc799c3c350130461d07
Andytjackson20 <31259001+andytjackson20@users.noreply.github.com>
2017-09-26 12:08:42 -0500

    Message Protocol set up

--- ColorCarvesApp/app/src/main/java/com/jackson/andrew/colorcarvesapp/BoardSpecsScreen.java
+++ ColorCarvesApp/app/src/main/java/com/jackson/andrew/colorcarvesapp/BoardSpecsScreen.java
@@ -3,6 +3,7 @@ package com.jackson.andrew.colorcarvesapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -11,39 +12,77 @@ import android.widget.TextView;
 
 public class BoardSpecsScreen extends AppCompatActivity {
 
-    public Button BoardSpecsConfirm;
-    public Button BoardSpecsCancel;
-    public CheckBox KeepWheelDiam;
-    public CheckBox KeepLEDSpacing;
-    public SeekBar WheelDiam;
-    public SeekBar LEDSpacing;
-    public TextView WheelDiamDisplay;
-    public TextView LEDSpacingDisplay;
+    private Button BoardSpecsConfirm;
+    private Button BoardSpecsCancel;
+    private CheckBox checkboxKeepWheelDiam;
+    private CheckBox checkboxKeepLEDSpacing;
+    private SeekBar seekbarWheelDiam;
+    private SeekBar seekbarLEDSpacing;
+    private TextView WheelDiamDisplay;
+    private TextView LEDSpacingDisplay;
+    private byte id = (byte)0x13;
+    private byte data2 = (byte)0x7F;
+    private byte data1 = (byte)0x03;
+    private byte data0 = (byte)0xFF;
+    private byte byteWheelDiam;
+    private byte[] ledSpacing;
+    private Payload payload;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_board_specs_setting);
 
-        KeepLEDSpacing = (CheckBox) findViewById(R.id.KeepLedSpacing);
-        KeepWheelDiam = (CheckBox) findViewById(R.id.KeepWheelDiam);
+        checkboxKeepLEDSpacing = (CheckBox) findViewById(R.id.KeepLedSpacing);
+        checkboxKeepWheelDiam = (CheckBox) findViewById(R.id.KeepWheelDiam);
         BoardSpecsConfirm = (Button) findViewById(R.id.BoardSpecsConfirm);
         BoardSpecsCancel = (Button) findViewById(R.id.BoardSpecsCancel);
-        WheelDiam = (SeekBar) findViewById(R.id.WheelDiamSeekbar);
-        LEDSpacing = (SeekBar) findViewById(R.id.LEDSpacingSeekbar);
+        seekbarWheelDiam = (SeekBar) findViewById(R.id.WheelDiamSeekbar);
+        seekbarLEDSpacing = (SeekBar) findViewById(R.id.LEDSpacingSeekbar);
         WheelDiamDisplay = (TextView) findViewById(R.id.WheelDiamDisplay);
         LEDSpacingDisplay= (TextView) findViewById(R.id.LEDSpacingDisplay);
 
 
-        WheelDiam.setEnabled(false); // Seekbar is not available unless checkbox is unchecked
-        LEDSpacing.setEnabled(false); // checkbox must be unchecked
+        seekbarWheelDiam.setEnabled(false); // Seekbar is not available unless checkbox is unchecked
+        seekbarLEDSpacing.setEnabled(false); // checkbox must be unchecked
 
-        CheckWheelSeekbar(WheelDiam);
-        CheckLEDSeekbar(LEDSpacing);
+        CheckWheelSeekbar(seekbarWheelDiam);
+        CheckLEDSeekbar(seekbarLEDSpacing);
+
+        ledSpacing = new byte[2];
+        payload = new Payload();
 
         BoardSpecsConfirm.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
+                if(checkboxKeepWheelDiam.isChecked())
+                {
+                    byteWheelDiam = (byte)0x7F;
+                }
+
+                if(!checkboxKeepWheelDiam.isChecked())
+                {
+                    byteWheelDiam = (byte)seekbarWheelDiam.getProgress();
+                }
+
+                if(checkboxKeepLEDSpacing.isChecked())
+                {
+                    ledSpacing[1] = (byte)0x03;
+                    ledSpacing[0] = (byte)0xFF;  //sets led spacing to 123, do not care, in protocol
+                }
+
+                if(!checkboxKeepLEDSpacing.isChecked())
+                {
+                    intToByteArray(ledSpacing,seekbarLEDSpacing.getProgress());
+
+                }
+
+                payload.id.setId(id);
+                payload.data.setData(2,(byte)(data2 & byteWheelDiam));
+                payload.data.setData(1, (byte)(data1 & ledSpacing[1]));
+                payload.data.setData(0,(byte)(data0 & ledSpacing[0]));
+                CMPPort.getInstance().queueToSend(payload);
                 ReturnToMainMenu();
             }
         });
@@ -56,15 +95,15 @@ public class BoardSpecsScreen extends AppCompatActivity {
         });
 
 
-        KeepWheelDiam.setOnClickListener(new View.OnClickListener() {
+        checkboxKeepWheelDiam.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                CheckStatusOfWheelDiam(KeepWheelDiam);
+                CheckStatusOfWheelDiam(checkboxKeepWheelDiam);
             }
             });
 
-        KeepLEDSpacing.setOnClickListener(new View.OnClickListener() {
+        checkboxKeepLEDSpacing.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                CheckStatusOfLEDSpacing(KeepLEDSpacing);
+                CheckStatusOfLEDSpacing(checkboxKeepLEDSpacing);
             }
         });
 
@@ -80,12 +119,12 @@ public class BoardSpecsScreen extends AppCompatActivity {
 
     public void DisplayWheelDiam(int wheeldiam) {
 
-        WheelDiamDisplay.setText(String.valueOf(wheeldiam));  // Display wheel diam divide by 10 to convert to decimal
+        WheelDiamDisplay.setText(String.valueOf(wheeldiam) + " mm");  // Display wheel diam divide by 10 to convert to decimal
     }
 
     public void DisplayLEDSpacing (int LEDSpacing){
         float ledspace = LEDSpacing;
-        LEDSpacingDisplay.setText(String.valueOf(ledspace/10));
+        LEDSpacingDisplay.setText(String.valueOf(ledspace/10) + " mm");
     }
 
 
@@ -146,19 +185,31 @@ public class BoardSpecsScreen extends AppCompatActivity {
 
         if (mcheckbox.isChecked()){
 
-            WheelDiam.setEnabled(false);
+            seekbarWheelDiam.setEnabled(false);
         }
         else{
-            WheelDiam.setEnabled(true);
+            seekbarWheelDiam.setEnabled(true);
         }
     }
 
     public void CheckStatusOfLEDSpacing(CheckBox mcheckbox){
         if(mcheckbox.isChecked()){
-            LEDSpacing.setEnabled(false);
+            seekbarLEDSpacing.setEnabled(false);
         }
         else{
-            LEDSpacing.setEnabled(true);
+            seekbarLEDSpacing.setEnabled(true);
         }
+    }
+
+    public void intToByteArray(byte [] val, int data)
+    {
+
+
+        val[0] = (byte)(data);  //discards all but bottom 8 bits of val
+        val[1] = (byte)((data >> 8 )); // takes val and discards bottom 8 bits.
+
+
+
+
     }
 }
